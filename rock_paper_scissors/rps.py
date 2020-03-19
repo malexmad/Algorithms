023f2aab7c50582337e4d6beb90c8efb29eacbd6
malex-01 <malex140@gmail.com>
2020-03-19 18:54:56 -0400

    completed rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,23 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  rps = [['rock'], ['paper'], ['scissors']]
+
+  combo = []
+  if n == 0:
+    return [[]]
+  if n == 1:
+    return rps
+
+  rounds = rock_paper_scissors(n-1)
+
+  for round in rounds:
+    for play in rps:
+
+      np = round+play
+      combo.append(np)
+  return combo
+
 
 
 if __name__ == "__main__":
